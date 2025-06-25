822dbbdcb76dd30caa1cb6903d9d0982a4327325
Manuel Rodriguez <rodriguez.manuelg4@gmail.com>
2025-06-25 16:23:44 -0500

    Camera Transition to Other Rooms

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -47,10 +48,10 @@ public class CameraControl : MonoBehaviour
     private void Update()
     {
         Rooms newRoom = GetRoomForPlayer(playerPos.position);
-/*        if(newRoom != null && newRoom != currentRoom && !isTransitioning)
+        if(newRoom != null && newRoom != currentRoom && !isTransitioning)
         {
             StartCoroutine(CameraTransition(newRoom));
-        }*/
+        }
 
         if(!isTransitioning && currentRoom != null)
         {
@@ -67,7 +68,7 @@ public class CameraControl : MonoBehaviour
             }
             else if (currentRoom.verticalRoom)
             {
-                //MoveCameraVertically();
+                MoveCameraVertically(currentRoom);
             }
             else
             {
@@ -94,9 +95,56 @@ public class CameraControl : MonoBehaviour
 
     }
 
+    private void MoveCameraVertically(Rooms room)
+    {
+        Vector2 minBounds = room.roomCenter - room.roomSize / 2;
+        Vector2 maxBounds = room.roomCenter + room.roomSize / 2;
+
+        Vector3 followPlayer = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+
+        float clampY = Mathf.Clamp(followPlayer.y, Mathf.Round(minBounds.y + CameraHalfHeight()), Mathf.Round(maxBounds.y - CameraHalfHeight()));
+
+        transform.position = new Vector3(room.roomCenter.x, clampY, transform.position.z);
+    }
+
     float CameraHalfWidth()
     {
         return Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    float CameraHalfHeight()
+    {
+        return Camera.main.orthographicSize;
+    }
+
+    IEnumerator CameraTransition(Rooms newRoom)
+    {
+        isTransitioning = true;
+
+        Vector3 currentPos = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+
+        Vector2 minBounds = newRoom.roomCenter - newRoom.roomSize / 2;
+        Vector3 maxBounds = newRoom.roomCenter + newRoom.roomSize / 2;
+
+        float clampX = Mathf.Clamp(currentPos.x, Mathf.Round(minBounds.x + CameraHalfWidth()), Mathf.Round(maxBounds.x - CameraHalfWidth()));
+        float clampY = Mathf.Clamp(currentPos.y, Mathf.Round(minBounds.y + CameraHalfHeight()), Mathf.Round(maxBounds.y - CameraHalfHeight()));
+
+        Vector3 targetPos = new Vector3(clampX, clampY, transform.position.z);
+        Vector3 startPos = transform.position;
+
+        float elapsedTime = 0;
+        float duration = Vector3.Distance(targetPos, startPos) / transitionSpeed;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, Mathf.SmoothStep(0f, 1f, elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        currentRoom = newRoom;
+        isTransitioning = false;
     }
 
 }
